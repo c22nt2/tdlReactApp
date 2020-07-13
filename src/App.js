b0d646f23673f3fe902b2e8acd308fccb5abcf12
Thuong DV <c22nt2@gmail.com>
2020-07-13 17:21:04 +0700

    change App.js

--- src/App.js
+++ src/App.js
@@ -15,12 +15,6 @@ function App() {
   return (
     <div className="App">
       <h1>App Components</h1>
-      <Demo></Demo>
-      <DM.Demo1></DM.Demo1>
-      <DM.Demo2></DM.Demo2>
-      <DM.Demo3></DM.Demo3>
-      <Parent></Parent>
-      <Sample></Sample>
       <ToDoList></ToDoList>
     </div>
   );
